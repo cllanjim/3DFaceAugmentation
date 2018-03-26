eafd1122e1cd2e2ab34f73ac6868c7f304fa5651
joaootavio93 <joao_otavio93@live.com>
2018-03-26 09:11:00 -0300

    Adding feature extraction test

--- FaceAugmentationLib/ApplicationTest/FeatureExtractionTest.cpp
+++ FaceAugmentationLib/ApplicationTest/FeatureExtractionTest.cpp
@@ -3,10 +3,17 @@
 int main()
 {
 	cv::Mat image = cv::imread("../../../Dataset/Images/Faces/0000003.jpg");
-	FeatureExtraction::ExtractorType extractorType = FeatureExtraction::ExtractorType::ORB;
-	FeatureExtraction::Feature feature = FeatureExtraction::Instance().ExtractFeatures(image.data, image.cols, image.rows, extractorType);
-	cv::imshow("descriptor", feature.descriptor);
+
+	FeatureExtraction::DetectorType detectorType = FeatureExtraction::DetectorType::ORB;
+	FeatureExtraction::DescriptorType descriptorType = FeatureExtraction::DescriptorType::ORB;
+	
+	FeatureExtraction::Feature feature = FeatureExtraction::Instance().ExtractFeatures(image.data, image.cols, image.rows, descriptorType, detectorType);
+
+	cv::drawKeypoints(image, feature.keypoints, image, cv::Scalar(0, 255, 0));
+
+	cv::imshow("keypoints", image);
 	cv::waitKey(0);
 	cv::destroyAllWindows();
+
 	return 0;
 }
--- FaceAugmentationLib/FeatureExtraction/FeatureExtraction.h
+++ FaceAugmentationLib/FeatureExtraction/FeatureExtraction.h
@@ -7,6 +7,7 @@
 #endif 
 
 #include <opencv2\features2d.hpp>
+#include "opencv2/xfeatures2d.hpp"
 #include <opencv2\opencv.hpp>
 #include <windows.h>
 
@@ -15,17 +16,27 @@ class FeatureExtraction
 public:
 	struct Feature {
 		std::vector<cv::KeyPoint> keypoints;
-		cv::Mat descriptor;
+		cv::Mat descriptors;
+		int keypointsSize;
+		int descWidth;
+		int descHeight;
 	};
 
-	enum ExtractorType{
-		ORB
+	enum class DetectorType 
+	{
+		AGAST, AKAZE, BRISK, FAST, GFFT, KAZE, MSDD, MSER, ORB, SBD, SIFT, STAR, SURF
+	};
+
+	enum class DescriptorType
+	{
+		AKAZE, BRIEF, BRISK, DAISY, FREAK, KAZE, LATCH, LUCID, ORB, SIFT, SURF
 	};
 
 	static FEATUREEXTRACTION_API FeatureExtraction& Instance();
 	FEATUREEXTRACTION_API FeatureExtraction();
-	FEATUREEXTRACTION_API FeatureExtraction::Feature ExtractFeatures(BYTE* pixels, int width, int height, ExtractorType extractorType);
+	FEATUREEXTRACTION_API FeatureExtraction::Feature ExtractFeatures(BYTE* pixels, int width, int height, DescriptorType descriptorType, DetectorType detectorType);
 
 private:
 	static FeatureExtraction* instance;
+	std::vector<cv::KeyPoint> DetectFeatures(BYTE* pixels, int width, int height, DetectorType detectorType);
 };
